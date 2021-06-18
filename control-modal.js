7cfaf6b208088b0a68010bd6e090deea294d1872
princecodes247 <princecodes247@gmail.com>
2021-06-17 20:37:56 -0400

    feat: update modal js

--- control-modal.js
+++ control-modal.js
@@ -1,4 +1,16 @@
+console.log("ko");
 let modal = document.querySelector(".modal");
 
 let openBtn = document.querySelector(".open-modal-btn");
 let closeBtn = modal.querySelector(".close-modal-btn");
+
+openBtn.addEventListener("click", ()=>{
+    event.preventDefault();
+    console.log("ji");
+    modal.classList.remove("close")
+})
+
+closeBtn.addEventListener("click", ()=>{
+    event.preventDefault();
+    modal.classList.add("close")
+})
