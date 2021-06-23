c7f1f21734da7c4ab3effc8bd44e3bb180cca0d7
princecodes247 <princecodes247@gmail.com>
2021-06-23 15:33:09 -0400

    feat: add sass files

---
+++ README.md
@@ -0,0 +1,15 @@
+FIX
+    --Header shrinking jaggeredly
+
+STYLE
+    Home
+    --Add block design elements to the home page
+    --Align grid items properly
+    --Stop stretching buttons
+    --Design newsletter form
+    --Add social icons
+    Dashboard
+    --Add collection theme
+    --Change collection grid to grid layout
+    --Add inputs
+    --Add Side menu icons
--- scroll_shrink.js
+++ scroll_shrink.js
@@ -1,14 +1,15 @@
 let target = document.querySelector(".scroll-shrink");
 let scrollPos = window.scrollY;
 let hheight = target.offsetHeight;
-
+console.log(hheight);
 // let body = document.b
 const add_class = () => target.classList.add("small");
 const remove_class = () => target.classList.remove("small");
 window.addEventListener("scroll", ()=>{
     scrollPos = window.scrollY
 
-    if (scrollPos >= hheight) {
+    if (scrollPos > hheight+200) {
+        
         add_class()
     } else {
         remove_class()
