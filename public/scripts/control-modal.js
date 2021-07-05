2a1c700305fb3b32f36690669e561eb4e231acf1
princecodes247 <princecodes247@gmail.com>
2021-07-05 19:37:42 -0400

    feat: control modal script

--- public/scripts/control-modal.js
+++ public/scripts/control-modal.js
@@ -1,13 +1,15 @@
 let modal = document.querySelector(".modal");
-let openBtn = document.querySelector(".open-modal-btn");
+let openBtns = document.querySelectorAll(".open-modal-btn");
 let closeBtn = modal.querySelector(".close-modal-btn");
 
-openBtn.addEventListener("click", ()=>{
-    event.preventDefault();
-    modal.classList.remove("close")
-})
-
-closeBtn.addEventListener("click", ()=>{
-    event.preventDefault();
-    modal.classList.add("close")
+openBtns.forEach(openBtn => {
+    openBtn.addEventListener("click", ()=>{
+        event.preventDefault();
+        modal.classList.remove("close")
+    })
+    
+    closeBtn.addEventListener("click", ()=>{
+        event.preventDefault();
+        modal.classList.add("close")
+    })
 })
