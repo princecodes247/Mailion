f14e7fe037bd5f50219e55ce97ecbc522c92a3cb
Prince Codes <princecodes247@gmail.com>
2021-11-14 18:23:46 +0100

    fix: JSON POST not working

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ app.set('layout', 'layouts/layout');
 app.set("layout extractStyles", true);
 
 // Express body parser
-// app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //Fix Session Warning
 
